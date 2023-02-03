af69845fb2f1e2ed10b9ac392c423d5212b2c4e1
Alek Evans <github@alekeagle.com>
2023-02-02 19:39:03 -0900

    re-reverse engineer home depot email API

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import parseArgs from './parseArgs.js';
-import { Worker } from 'worker_threads';
-import { readFile } from 'fs/promises';
+import parseArgs from "./parseArgs.js";
+import { Worker } from "worker_threads";
+import { readFile } from "fs/promises";
 
 let successCount = 0;
 const args = parseArgs();
@@ -10,21 +10,19 @@ const settings: {
   fromEmail: string;
   count: number;
   sku: string;
-  storeId: string;
   threads: number;
   email: string;
 } = {
   fromEmail: args.flags.e || args.flags.email || null,
-  count: parseInt(args.flags.c || args.flags.count || '5'),
-  sku: args.flags.s || args.flags.sku || '316369949',
-  storeId: args.flags.S || args.flags.store || '2676',
-  threads: parseInt(args.flags.t || args.flags.threads || '1'),
-  email: args.args[1]
+  count: parseInt(args.flags.c || args.flags.count || "5"),
+  sku: args.flags.s || args.flags.sku || "316369949",
+  threads: parseInt(args.flags.t || args.flags.threads || "1"),
+  email: args.args[1],
 };
 
 async function doThing() {
-  const packageJSON = JSON.parse(await readFile('./package.json', 'utf8'));
-  if (args.args.length < 2 || args.flags.h === '' || args.flags.help === '') {
+  const packageJSON = JSON.parse(await readFile("./package.json", "utf8"));
+  if (args.args.length < 2 || args.flags.h === "" || args.flags.help === "") {
     console.log(
       `yougotanitem v${packageJSON.version} by ${packageJSON.author}
 
@@ -34,30 +32,28 @@ Usage: yougotanitem email
   -e, --email       Specify a specific email instead of random garbage.
   -c, --count       How many emails should be sent (default: 5).
   -s, --sku         Specify the item shared (default: 316369949).
-  -S, --store       Specify the store the item comes from  (default: 2676).
   -t, --threads     How many threads to use (default: 1).`
     );
     return;
   }
 
   function spawnThread(threadID: number) {
-    const worker = new Worker('./dist/worker.js', {
+    const worker = new Worker("./dist/worker.js", {
       workerData: {
         fromEmail: settings.fromEmail,
         count: settings.count,
         sku: parseInt(settings.sku),
-        storeId: parseInt(settings.storeId),
         threadID,
         threadCount: settings.threads,
         email: args.args[1],
-        successCount
-      }
+        successCount,
+      },
     });
     allWorkers[threadID] = worker;
-    worker.on('message', (message: { threadID: number }) => {
+    worker.on("message", (message: { threadID: number }) => {
       if (message.threadID === threadID) {
         successCount++;
-        allWorkers.forEach(w => {
+        allWorkers.forEach((w) => {
           w.postMessage({ successCount, threadID: message.threadID });
         });
         console.log(
@@ -72,10 +68,10 @@ Usage: yougotanitem email
         }
       }
     });
-    worker.on('error', err => {
+    worker.on("error", (err) => {
       console.error(err);
     });
-    worker.on('exit', code => {
+    worker.on("exit", (code) => {
       if (code !== 0) {
         console.error(`Worker stopped with exit code ${code}`);
         spawnThread(threadID);
--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,4 @@
-import { workerData, parentPort } from 'worker_threads';
-
-import fetch from 'node-fetch';
+import { workerData, parentPort } from "worker_threads";
 
 let {
   fromEmail,
@@ -10,7 +8,7 @@ let {
   threadID,
   threadCount,
   email,
-  successCount
+  successCount,
 }: {
   fromEmail: string | null;
   count: number;
@@ -23,50 +21,61 @@ let {
 } = workerData;
 
 function makeString(len: number): string {
-  let str = '';
+  let str = "";
   for (let i = 0; i < len; i++) {
     str += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
   return str;
 }
-
 async function send() {
-  const fromAddress =
-      workerData.fromEmail || `${makeString(10)}@${makeString(10)}.com`,
-    res = await fetch('https://www.homedepot.com/p/svcs/sendProductEmail', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-      },
-      body: JSON.stringify({
-        fromAddress,
-        toAddress: email,
-        sku: sku,
-        storeId: storeId
-      })
-    });
+  const fromAddress = fromEmail || `${makeString(10)}@${makeString(10)}.com`,
+    res = await fetch(
+      "https://www.homedepot.com/federation-gateway/graphql?opname=emailProduct",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+          "x-experience-name": "general-merchandise",
+        },
+        body: JSON.stringify({
+          operationName: "emailProduct",
+          variables: {
+            fromAddress,
+            toAddress: email,
+            itemId: sku,
+          },
+          query:
+            "query emailProduct($fromAddress: String!, $toAddress: String!, $itemId: String!) {\n  emailProduct(fromAddress: $fromAddress, toAddress: $toAddress, itemId: $itemId) {\n    responseMessageId\n    message\n    __typename\n  }\n}\n",
+        }),
+      }
+    ),
+    json = await res.json();
 
-  if (res.status !== 204) {
-    throw new Error(`${res.status} ${await res.text()}`);
+  if (res.status !== 200 && json.data.emailProduct?.message !== "Success") {
+    console.error(json);
+    throw new Error(`${res.status}`);
   } else {
     parentPort.postMessage({ threadID });
   }
 }
 
-parentPort.on('message', async (message: { successCount: number; threadID: number }) => {
-  successCount = message.successCount;
-  if (successCount === count)
-    process.exit(0);
-  else if (message.threadID === threadID) {
-    try {
-      await send();
-    } catch (err) {
-      console.error(err);
-      process.exit(1);
+parentPort.on(
+  "message",
+  async (message: { successCount: number; threadID: number }) => {
+    successCount = message.successCount;
+    if (successCount === count) process.exit(0);
+    else if (message.threadID === threadID) {
+      try {
+        await send();
+      } catch (err) {
+        console.error(err);
+        process.exit(1);
+      }
     }
   }
-});
+);
 
 try {
   await send();
